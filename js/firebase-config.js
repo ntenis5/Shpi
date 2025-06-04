532b9e63d5cad53804dbdf5e12e01402daf4ac56
ntenis5 <nteniskotsiou@gmail.com>
2025-06-04 18:20:41 +0200

    Update and rename firebase-config.js to js/firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,14 @@
-// firebase-config.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "API_YT",
+  apiKey: "API_YT_KETU",
   authDomain: "projekti.firebaseapp.com",
   projectId: "projekti",
   storageBucket: "projekti.appspot.com",
-  messagingSenderId: "000000000",
-  appId: "APP_ID"
+  messagingSenderId: "000000000000",
+  appId: "APP_ID_YT"
 };
 
 const app = initializeApp(firebaseConfig);
