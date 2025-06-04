002d38423446b80c39435e4d070379a0c0baced9
ntenis5 <nteniskotsiou@gmail.com>
2025-06-04 18:31:56 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+// /js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getFirestore, collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "API_YT_KETU",
-  authDomain: "projekti.firebaseapp.com",
-  projectId: "projekti",
-  storageBucket: "projekti.appspot.com",
-  messagingSenderId: "000000000",
-  appId: "APP_ID_YT"
+  apiKey: "AIzaSyAzeg2ha8agdS5zKFB34Udwj2CTMJamy0E",
+  authDomain: "shpi-d4e2d.firebaseapp.com",
+  projectId: "shpi-d4e2d",
+  storageBucket: "shpi-d4e2d.appspot.com",
+  messagingSenderId: "600471884377",
+  appId: "1:600471884377:web:5512544dc9dfb1fa009748"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const propertyCol = collection(db, "properties");
